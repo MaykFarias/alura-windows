b48ffd04a366d9c98f2c19c7bdc4fdfe384ee96d
Mayk Farias <maykfariasy@gmail.com>
2026-05-12 19:49:48 -0300

    estrutura inicial do repositorio

---
+++ modulo-2/exercícios/desafio-criando-um-script-compactador/arquivos-compactados/compactador.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+echo Compactando os arquivos PDF. 
+
+tar -cf arquivos-compactados.zip *.pdf
---
+++ modulo-2/exercícios/desafio-otimizando-um-script-de-backup/script.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Copiando fotos da sua câmera para o computador.
+copy .\camera-photos\*.jpg .\fotos-backup
