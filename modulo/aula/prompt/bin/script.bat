9306cc07bbc0eeb072e8c6e744458c368cd3092a
Mayk Farias <maykfariasy@gmail.com>
2026-05-14 20:26:55 -0300

    Curso finalizado

---
+++ modulo/aula/prompt/bin/script.bat
@@ -0,0 +1,8 @@
+@echo off
+rem Limpando
+cls
+set /p nome=Digite seu nome= 
+set /p email=Digite seu e-mail= 
+pause 
+echo ................................
+echo Seu nome é %nome% e seu e-mail é %email%
